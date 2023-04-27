1c19eb241e2f6a02703c61b34f88b04b288d2939
Avinash <kumarpallko@gmail.com>
2023-04-27 15:42:12 +0530

    First changes done.

--- nui-creative-portfolio-html-website-template-2023-03-12-17-37-46-utc/Nui - template package/UPLOAD/nui/assets/js/theme.js
+++ nui-creative-portfolio-html-website-template-2023-03-12-17-37-46-utc/Nui - template package/UPLOAD/nui/assets/js/theme.js
@@ -3290,6 +3290,49 @@
 		$(this).trigger("hover");
 	});
 
+	var swiper = new Swiper(".mySwiper", {
+		slidesPerView: 1,
+		spaceBetween: 0,
+		autoplay:{
+			delay:2500,
+		},
+		pagination: {
+		  el: ".swiper-pagination",
+		  clickable: true,
+		},
+	  });
 
+
+	  var swiper = new Swiper(".slide-content", {
+		slidesPerView: 3,
+		spaceBetween: 25,
+		loop: true,
+		centerSlide: 'true',
+		fade: 'true',
+		grabCursor: 'true',
+		pagination: {
+		  el: ".swiper-pagination",
+		  clickable: true,
+		  dynamicBullets: true,
+		},
+		navigation: {
+		  nextEl: ".swiper-button-next",
+		  prevEl: ".swiper-button-prev",
+		},
+	
+		breakpoints:{
+			0: {
+				slidesPerView: 1,
+			},
+			520: {
+				slidesPerView: 2,
+			},
+			950: {
+				slidesPerView: 3,
+			},
+		},
+	  });
+	 
+	
 
 })(jQuery); 
